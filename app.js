dedfaf6777f1fac6c6e376a62df5f3b066582226
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-19 02:56:27 +1100

    adjusted layout on campgrounds and increased dummy data

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ let campgrounds = [
     image:
       'https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104497f1c27da6e9b1bd_340.jpg',
   },
+  {
+    name: 'Salmon Creek',
+    image:
+      'https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f3c578a4eeb5_340.jpg',
+  },
+  {
+    name: 'Granite Hill',
+    image:
+      'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f1c27da6e9b1bd_340.jpg',
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image:
+      'https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104497f1c27da6e9b1bd_340.jpg',
+  },
 ];
 // Routes
 app.get('/', (req, res) => {
