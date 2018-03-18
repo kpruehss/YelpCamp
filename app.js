7daf2b5edec9b4aa8c25ab50cee06543179249f4
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-18 13:44:33 +1100

    started setting up routes

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const app = express();
+
+// Set render engine
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  res.render('campgrounds');
+});
+
+// Server instance
+app.listen(3000, () => {
+  console.log('yelpCamp Server started');
+});
