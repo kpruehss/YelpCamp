6c6d10d23d5df3b1ba790501a7ad8b1fc830bc3c
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-18 14:11:13 +1100

    added campgrounds dummy data to test campgrounds.ejs

--- app.js
+++ app.js
@@ -13,15 +13,18 @@ app.get('/campgrounds', (req, res) => {
   const campgrounds = [
     {
       name: 'Salmon Creek',
-      image: 'https://pixabay.com/en/travel-adventure-camping-night-2604981/',
+      image:
+        'https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144394f3c578a4eeb5_340.jpg',
     },
     {
       name: 'Granite Hill',
-      image: 'https://pixabay.com/en/tent-camping-remote-campsite-1208201/',
+      image:
+        'https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f1c27da6e9b1bd_340.jpg',
     },
     {
       name: 'Mountain Goat\'s Rest',
-      image: 'https://pixabay.com/en/night-stars-galaxy-wonder-camp-839807/',
+      image:
+        'https://pixabay.com/get/eb30b90e2af0033ed1584d05fb1d4e97e07ee3d21cac104497f1c27da6e9b1bd_340.jpg',
     },
   ];
   res.render('campgrounds', {campgrounds: campgrounds});
