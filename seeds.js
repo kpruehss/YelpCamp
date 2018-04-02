ff1e88c31746043b94562675fc2127da1d9e7a21
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-04-02 15:12:05 +1000

    Added show/description tags from DB

--- seeds.js
+++ seeds.js
@@ -7,19 +7,22 @@ const data = [
     name: 'Cloud\'s Rest',
     image:
       'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c85daa025ee04c951b6ac12fe3ba031a&auto=format&fit=crop&w=500&q=60',
-    description: 'Stuff goes here',
+    description:
+      'Jerky beef ribs qui bresaola, velit laborum prosciutto.  Salami anim picanha ball tip, tri-tip pancetta adipisicing consequat cow labore andouille.  Ground round ut occaecat pork loin tongue aliquip et.  Ut in ex pariatur andouille leberkas culpa.  Brisket cupim pork chop quis, beef short ribs bresaola aliqua flank ea meatloaf.  Lorem ham fatback shank nulla.  Esse nisi salami sint.',
   },
   {
     name: 'Grand Campyon',
     image:
       'https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=500&q=60',
-    description: 'More Stuff goes here',
+    description:
+      'Jerky beef ribs qui bresaola, velit laborum prosciutto.  Salami anim picanha ball tip, tri-tip pancetta adipisicing consequat cow labore andouille.  Ground round ut occaecat pork loin tongue aliquip et.  Ut in ex pariatur andouille leberkas culpa.  Brisket cupim pork chop quis, beef short ribs bresaola aliqua flank ea meatloaf.  Lorem ham fatback shank nulla.  Esse nisi salami sint.',
   },
   {
     name: 'Lightshow Woods',
     image:
       'https://images.unsplash.com/photo-1465695954255-a262b0f57b40?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=06d92f647a2937af54f658e199c3d990&auto=format&fit=crop&w=500&q=60',
-    description: 'Even more Stuff goes here',
+    description:
+      'Jerky beef ribs qui bresaola, velit laborum prosciutto.  Salami anim picanha ball tip, tri-tip pancetta adipisicing consequat cow labore andouille.  Ground round ut occaecat pork loin tongue aliquip et.  Ut in ex pariatur andouille leberkas culpa.  Brisket cupim pork chop quis, beef short ribs bresaola aliqua flank ea meatloaf.  Lorem ham fatback shank nulla.  Esse nisi salami sint.',
   },
 ];
 
